78b1226ea823299cd7d3c051222f702a05efea5b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-07 14:21:11 +0530

    corrected the syntax error in App .java

--- examples/example1/src/main/java/com/daniyal/springcore/App.java
+++ examples/example1/src/main/java/com/daniyal/springcore/App.java
@@ -3,10 +3,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String []args)
-    [
+    {
         ApplicationContext applicationContext=new ClassPathXmlApplicationContext("beans.xml");
 
-        MessageApp messageApp=context.getBean("messageApp",MessageApp.class);
+        MessageApp messageApp=applicationContext.getBean("messageApp",MessageApp.class);
         messageApp.printMessage();
-    ]
+    }
 }
