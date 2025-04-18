17d1b6d46461a643f27e63cb3167a4f8936ec570
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-18 14:45:55 +0530

    Car.java created

--- examples/example2/src/main/java/com/daniyal/springcore/Car.java
+++ examples/example2/src/main/java/com/daniyal/springcore/Car.java
@@ -1,4 +1,10 @@
 package com.daniyal.springcore;
 public class Car {
     @AutoWired
+    private Engine engine;
+    public void drive()
+    {
+        engine.start();
+        System.out.println("Car is moving....");
+    }
 }
