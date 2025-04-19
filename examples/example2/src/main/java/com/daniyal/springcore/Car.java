a01c1dcf9da7ea17ca906ff10a877c5246145d4f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-19 22:36:09 +0530

    chanegd the Car code for byName and removed constructor and added setters

--- examples/example2/src/main/java/com/daniyal/springcore/Car.java
+++ examples/example2/src/main/java/com/daniyal/springcore/Car.java
@@ -3,13 +3,18 @@ public class Car {
         private Engine engine;
         private Tyre tyre;
         private Glass glass;
-        public Car(Engine engine,Tyre tyre,Glass glass)
+        public void setEngine(Engine engine)
         {
             this.engine=engine;
-            this.tyre=tyre;
+        }
+        public void setGlass(Glass glass)
+        {
             this.glass=glass;
         }
-    
+        public void setTyre(Tyre tyre)
+        {
+            this.tyre= tyre;
+        }
     public void drive()
     {
         engine.start();
