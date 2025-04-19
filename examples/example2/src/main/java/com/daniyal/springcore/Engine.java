40a7139d6ab846b96b59c99f0a10d44efcbcd154
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-19 21:37:50 +0530

    made the changes for autowring example

--- examples/example2/src/main/java/com/daniyal/springcore/Engine.java
+++ examples/example2/src/main/java/com/daniyal/springcore/Engine.java
@@ -1,12 +1,8 @@
 package com.daniyal.springcore;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-@Component
 public class Engine
 {
     private String type;
-    @Autowired
     public Engine(String type)
     {
         this.type=type;
