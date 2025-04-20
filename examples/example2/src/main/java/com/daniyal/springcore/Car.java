f40d2282e52991ae76f1d238daba8e51fc63558d
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-20 21:12:22 +0530

    Updated Car.java

--- examples/example2/src/main/java/com/daniyal/springcore/Car.java
+++ examples/example2/src/main/java/com/daniyal/springcore/Car.java
@@ -1,25 +1,25 @@
 package com.daniyal.springcore;
 public class Car {
-        private Engine engine;
-        private Tyre tyre;
+        private Engine myEngine;
+        private Tyre truck;
         private Glass glass1;
-        public void setEngine(Engine engine)
+        public void setDaniyal(Engine myEngine)
         {
-            this.engine=engine;
+            this.myEngine=myEngine;
         }
         public void setGlass(Glass glass)
         {
             this.glass1=glass;
         }
-        public void setTyre(Tyre tyre)
+        public void setTyre(Tyre tyre1)
         {
-            this.tyre= tyre;
+            this.truck= tyre1;
         }
     public void drive()
     {
-        engine.start();
-        tyre.rotate();
-        glass.move();
+        myEngine.start();
+        truck.rotate();
+        glass1.move();
         System.out.println("Car is moving....");
     }
 }
